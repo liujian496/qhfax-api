24eaef2fa26f6fb97bce1469a46b30a72986c436
liujian496 <liujian496@163.com>
2018-03-10 22:47:04 +0800

    submit all file

--- src/main/java/com/qhfax/api/App.java
+++ src/main/java/com/qhfax/api/App.java
@@ -2,18 +2,11 @@ package com.qhfax.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
-public class App extends SpringBootServletInitializer {
-
+public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(App.class);
     }
 }
