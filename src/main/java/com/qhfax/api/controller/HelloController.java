d292c5ec43ff37da6df5c52d9de36f8bc5ac2c6c
liujian496 <liujian496@163.com>
2018-03-14 15:40:07 +0800

    submit all file

--- src/main/java/com/qhfax/api/controller/HelloController.java
+++ src/main/java/com/qhfax/api/controller/HelloController.java
@@ -1,13 +1,19 @@
 package com.qhfax.api.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    private static final Logger logger = LogManager.getLogger(HelloController.class);
+
     @RequestMapping("/hello")
     public String sayHello(){
+        logger.info("=== Hello, qhfax-api ===");
         return ("Hello, qhfax-api");
     }
 }
